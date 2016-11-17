006af0644427dcfa32ac02c0dd01de9914ed0a6c
Nicolas Quiroz <nh.quiroz@gmail.com>
2016-11-17 02:41:35 -0300

    Add bot executable

---
+++ bin/bot.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/**
+ * WikiBot launcher script.
+ *
+ * @author Nicolas Quiroz <nh.quiroz@gmail.com>
+ */
+
+var WikiBot = require('../lib/wikibot');
+
+/**
+ * Environment variables used to configure the bot:
+ *
+ *  BOT_API_KEY: the authentication token to allow the bot to connect to your slack organization.
+ *  BOT_NAME: the username you want to give to the bot within your organization.
+ */
+var token = process.env.BOT_API_KEY || require('../token');
+var name = process.env.BOT_NAME;
+
+var wikibot = new WikiBot({
+    token: token,
+    name: name
+});
+
+wikibot.run();
